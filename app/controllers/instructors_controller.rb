e9e425adce737390b884c9db96a2654ab2f560a4
Mashal <azhar.mashal@gmail.com>
2018-05-04 11:01:45 -0400

    Instructors show on route

--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -1,9 +1,11 @@
 class InstructorsController < ApplicationController
     def index
        puts "This is the Instructors Controller"
+       @instructors = Instructor.all 
     end
 
     def show
        puts "This will show specific instructors"
+       @current_instructor = Instructor.find(params[:id])
     end 
 end
